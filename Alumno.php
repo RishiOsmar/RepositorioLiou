3a3b941a7c039fc8b58cd82465f45324eb807f33
RishiOsmar <60679732+RishiOsmar@users.noreply.github.com>
2020-02-10 14:54:49 -0600

    Add files via upload

---
+++ Alumno.php
@@ -0,0 +1,29 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Ejercico 16</title>
+</head>
+<body>
+<h2>Desarrollar lo siguiente 2: </h2>
+
+<p>
+Construya un pseucodigo que ingresada la nota de un alumnno si esta 
+es menor o igual que 10 muestre DESAPROBADO
+</p>
+
+<?php
+if ($nota <= 10) {
+    echo "Su nota es APROBADA";
+
+} else {
+    echo "desaprobado";
+}
+
+
+
+
+?>
+
+</body>
+</html>
